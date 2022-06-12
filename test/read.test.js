a6527ad4d2baadd34013dda5df8605d0f78f848f
Lucas <lucascpinheiro10@gmail.com>
2022-06-12 18:18:25 -0300

    add mongoose skip and limit query modifiers example

--- test/read.test.js
+++ test/read.test.js
@@ -2,11 +2,14 @@ import assert from 'assert'
 import User from '../src/user.js'
 
 describe('Reading users out of the db', () => {
-  let joe
+  let joe, maria, alex, zach
 
   beforeEach((done) => {
     joe = new User({ name: 'Joe' })
-    joe.save()
+    maria = new User({ name: 'Maria' })
+    alex = new User({ name: 'Alex' })
+    zach = new User({ name: 'Zach' })
+    Promise.all([joe.save(), alex.save(), maria.save(), zach.save()])
       .then(() => done())
   })
 
@@ -22,6 +25,19 @@ describe('Reading users out of the db', () => {
     User.findOne({ _id: joe._id })
       .then((user) => {
         assert(user.name === 'Joe')
+        done()
+      })
+  })
+
+  it('can skip and limit the result set', (done) => {
+    User.find({}) // sem filtros no find
+      .sort({ name: 1 }) // ordena por nome
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2)
+        assert(users[0]._id.toString() === joe._id.toString())
+        assert(users[1]._id.toString() === maria._id.toString())
         done()
       })
   })
